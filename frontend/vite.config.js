d96fe39fde70ac96b077e9cc31d09fd6bf24d01b
mohanad66 <mohanadmahmoud33245@gmail.com>
2025-12-11 12:15:37 +0200

    editsone importing from .env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_API_URL,
+        target: import.meta.env.VITE_API_URL || 'http://localhost:8000',
         changeOrigin: true,
         secure: false,
         ws: true,
