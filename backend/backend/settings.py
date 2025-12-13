c38dd3cbb46395406fc9bbc785bf477cc1470379
mohanad66 <mohanadmahmoud33245@gmail.com>
2025-12-13 13:13:26 +0200

    fixing CSRF

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -118,7 +118,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 ASGI_APPLICATION = 'backend.asgi.application'
-
+CSRF_TRUSTED_ORIGINS = ['https://*']
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
