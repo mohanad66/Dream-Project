0bba87cc49c84f4ce0a2b97ee5c1617f940e8906
mohanad mahmoud fathy mohammed <116345386+mohanad66@users.noreply.github.com>
2026-01-12 03:20:54 +0200

    Change server host to '0.0.0.0'
    Updated the host in the serve function to allow external access.

--- backend/start.py
+++ backend/start.py
@@ -1,8 +1,7 @@
-# Dream-Project/backend/start.py
 import os
 from waitress import serve
-# *** IMPORTANT: Change this line to the correct module path ***
-from backend.backend.wsgi import application 
+from backend.backend.wsgi import application
 
 port = int(os.environ.get('PORT', 8000))
-serve(application, host='127.0.0.1', port=port)
+# Change host to '0.0.0.0'
+serve(application, host='0.0.0.0', port=port)
