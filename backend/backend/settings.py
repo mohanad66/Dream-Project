a10b2047cc93b0a8ec70d0fd47eb56814d82c51a
mohanad66 <mohanadmahmoud33245@gmail.com>
2026-08-17 15:49:38 +0300

    fix: add DEFAULT_RENDERER_CLASSES for production DEBUG=False

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -105,6 +105,9 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_RENDERER_CLASSES": (
+        "rest_framework.renderers.JSONRenderer",
+    ),
 }
 TEMPLATES = [
     {
